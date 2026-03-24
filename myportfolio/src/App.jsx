c89cd96bd9e2ac34356c3d7222d0e10a35f30c4c
Sushainagowda <csushaina@gmail.com>
2026-03-24 19:45:09 +0530

    save all

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,26 +1,18 @@
-import React from 'react'
-import { 
-  Navbar, 
-  Nav, 
-  Container, 
-  Row, 
-  Col, 
-  Card, 
-  Button, 
-  Badge, 
-  Form 
-} from 'react-bootstrap';
-import Navigation from ' ./components/Navigation';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Hero from './components/Hero';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 export default function App() {
   return (
-    <div>
+    <>
       <Navigation />
       <Hero />
       <Projects />
       <Contact />
-    </div>
-  )
+    </>
+  );
 }
